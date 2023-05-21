af85dcdaf523b0856e1638f0869377bce7142f0e
Robert Engel <robert.engel@apollidon.com>
2023-05-21 18:30:04 -0400

    Rae initial commit.

---
+++ src/app/fonts.ts
@@ -0,0 +1,7 @@
+import { Montserrat } from 'next/font/google';
+
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+});
